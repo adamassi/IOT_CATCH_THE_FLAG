cbc2742be50a079df2133afbbe02ef37b70f77a8
adamassi <adamassi@campus.technion.ac.il>
2026-06-26 22:12:09 +0300

    Refactor light color cycling in test_peach_request to limit colors and ensure proper termination

--- test_peach_requst.py
+++ test_peach_requst.py
@@ -10,6 +10,7 @@ send_lights_color_request(ColorBank.OFF)
 start_time = time.time()
 duration=5
 while time.time() - start_time < duration:
-        for color in [ColorBank.RED, ColorBank.GREEN, ColorBank.BLUE, ColorBank.YELLOW, ColorBank.PURPLE, ColorBank.CYAN, ColorBank.WHITE]:
+        for color in [ColorBank.RED, ColorBank.GREEN, ColorBank.BLUE]:
             send_lights_color_request(color)
             time.sleep(0.15)
+send_lights_color_request(ColorBank.OFF)
